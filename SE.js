893c9b409713f1784085c7f68940d4ff2adf8bdc
YashM8329 <145220340+YashM8329@users.noreply.github.com>
2024-11-11 15:32:29 +0530

    Add files via upload

---
+++ SE.js
@@ -0,0 +1,14 @@
+document.querySelectorAll(".scroll-button").forEach((button) => {
+  button.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    target.scrollIntoView({ behavior: "smooth" });
+  });
+});
+document.querySelectorAll(".nav-button").forEach((button) => {
+  button.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    target.scrollIntoView({ behavior: "smooth" });
+  });
+});
